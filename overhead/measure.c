810aeef5b876a5277bdd607b9107044b51cbfb78
Yunjae Lee <lyj7694@gmail.com>
2019-09-24 13:11:25 +0900

    Implement object initialization
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- overhead/measure.c
+++ overhead/measure.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define PAGE_SIZE		4096
+
 enum access_type {
 	SEQUENTIAL = 0,
 	RANDOM,
@@ -37,8 +39,52 @@ void perf_init(void)
 {
 }
 
-void init_object(void)
+void init_object(int **objp, long size, int access_type)
 {
+	int i, swap_with;
+	int from, to;
+	int nr_entries;
+	int temp;
+	int *access_seq;
+	int *object = *objp;
+
+	if (posix_memalign((void **)&object, PAGE_SIZE, size)) {
+		printf("Object allocation failed!\n");
+		exit(1);
+	}
+
+	memset(object, 0, size);
+
+	nr_entries = size / sizeof(int);
+	if (access_type == SEQUENTIAL) {
+		for (i = 0; i < nr_entries; i++)
+			object[i] = (i + 1) % nr_entries;
+	} else {
+		srand(42);
+		access_seq = malloc(size);
+		for (i = 0; i < nr_entries; i++)
+			access_seq[i] = i;
+
+		/* Randomize access sequence */
+		for (i = 0; i < nr_entries; i++) {
+			swap_with = rand() % nr_entries;
+			if (i == swap_with)
+				continue;
+			temp = access_seq[i];
+			access_seq[i] = access_seq[swap_with];
+			access_seq[swap_with] = temp;
+		}
+
+		/* Write access sequence to object */
+		from = access_seq[nr_entries - 1];
+		for (i = 0; i < nr_entries; i++) {
+			to = access_seq[i];
+			object[from] = to;
+			from = to;
+		}
+
+		free(access_seq);
+	}
 }
 
 void perf_record_start(void)
@@ -59,12 +105,19 @@ void perf_report(void)
 
 int main(int argc, char **argv)
 {
+	int *object;
+	long size;
+	int access_type;
 	struct input_args args;
+
 	if (handle_args(argc, argv, &args))
 		return -1;
 
+	size = args.size;
+	access_type = args.access_type;
+
 	perf_init();
-	init_object();
+	init_object(&object, size, access_type);
 	perf_record_start();
 	access_object();
 	perf_record_end();
