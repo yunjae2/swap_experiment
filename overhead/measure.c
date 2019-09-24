2f85ae116ea4cf9bddbea414c42fb4ac6e9c472f
Yunjae Lee <lyj7694@gmail.com>
2019-09-24 12:38:22 +0900

    Implement handling of input arguments
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- overhead/measure.c
+++ overhead/measure.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int handle_args(int argc, char **argv)
+struct input_args {
+	long size;		// object size
+	char *access_type;	// seq or rand
+};
+
+int handle_args(int argc, char **argv, struct input_args *args)
 {
+	if (argc != 3)
+		goto error;
+	if (strcmp(argv[1], "seq") && strcmp(argv[1], "rand"))
+		goto error;
+
+	args->size = atol(argv[2]) * 1024 * 1024;
+	args->access_type = malloc(sizeof(argv[1]));
+	strcpy(args->access_type, argv[1]);
+
 	return 0;
+
+error:
+	printf("Usage: %s <seq|rand> <object size (MiB)>\n", argv[0]);
+	return -1;
 }
 
 void perf_init(void)
@@ -31,7 +51,8 @@ void perf_report(void)
 
 int main(int argc, char **argv)
 {
-	if (handle_args(argc, argv))
+	struct input_args args;
+	if (handle_args(argc, argv, &args))
 		return -1;
 
 	perf_init();
@@ -40,4 +61,6 @@ int main(int argc, char **argv)
 	access_object();
 	perf_record_end();
 	perf_report();
+
+	return 0;
 }
