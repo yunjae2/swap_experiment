4509a5b00a44a12416c658bb535a706d213aec77
Yunjae Lee <lyj7694@gmail.com>
2019-09-25 07:47:21 +0900

    Trace number of page faults during object access
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- overhead/measure.c
+++ overhead/measure.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
+#include <asm/unistd.h>
+#include <linux/perf_event.h>
+#include <sys/ioctl.h>
 
 #define PAGE_SIZE		4096
 
@@ -14,6 +18,23 @@ struct input_args {
 	long size;		// object size
 	int access_type;	// seq or rand
 };
+
+struct perf_objects {
+	int nr_objects;		// up to 10
+	int fd[10];
+	char desc[10][30];	// description for each object
+};
+
+long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
+		int cpu, int group_fd, unsigned long flags)
+{
+	int ret;
+
+	ret = syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd,
+			flags);
+	return ret;
+}
+
 
 void print_interval(struct timespec *start, struct timespec *end)
 {
@@ -52,8 +73,39 @@ error:
 	return -1;
 }
 
-void perf_init(void)
+int perf_init_object(__u32 type, __u64 config, int cpu)
 {
+	struct perf_event_attr pe;
+	int fd;
+
+	memset(&pe, 0, sizeof(struct perf_event_attr));
+	pe.type = type;
+	pe.size = sizeof(struct perf_event_attr);
+	pe.disabled = 1;
+	pe.exclude_hv = 1;
+	pe.config = config;
+
+	fd = perf_event_open(&pe, 0, cpu, -1, 0);
+	if (fd == -1) {
+		fprintf(stderr, "Error opening leader %llx\n", pe.config);
+		exit(EXIT_FAILURE);
+	}
+
+	ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+
+	return fd;
+}
+
+
+struct perf_objects perf_init(void)
+{
+	struct perf_objects po;
+	po.nr_objects = 1;
+	strcpy(po.desc[0], "nr_page_faults");
+	po.fd[0] = perf_init_object(PERF_TYPE_SOFTWARE,
+			PERF_COUNT_SW_PAGE_FAULTS, 0);
+
+	return po;
 }
 
 void init_object(int **objp, long size, int access_type)
@@ -112,8 +164,12 @@ void init_object(int **objp, long size, int access_type)
 	print_interval(&start, &end);
 }
 
-void perf_record_start(void)
+void perf_record_start(struct perf_objects *po)
 {
+	int i;
+
+	for (i = 0; i < po->nr_objects; i++)
+		ioctl(po->fd[i], PERF_EVENT_IOC_ENABLE, 0);
 }
 
 void access_object(int *object, int size)
@@ -133,12 +189,24 @@ void access_object(int *object, int size)
 	print_interval(&start, &end);
 }
 
-void perf_record_end(void)
+void perf_record_end(struct perf_objects *po)
 {
+	int i;
+
+	for (i = 0; i < po->nr_objects; i++)
+		ioctl(po->fd[i], PERF_EVENT_IOC_DISABLE, 0);
 }
 
-void perf_report(void)
+void perf_report(struct perf_objects *po)
 {
+	int i;
+	long long count;
+
+	for (i = 0; i < po->nr_objects; i++) {
+		read(po->fd[i], &count, sizeof(long long));
+		printf("%s: %lld\n", po->desc[i], count);
+		close(po->fd[i]);
+	}
 }
 
 int main(int argc, char **argv)
@@ -147,6 +215,7 @@ int main(int argc, char **argv)
 	long size;
 	int access_type;
 	struct input_args args;
+	struct perf_objects po;
 
 	if (handle_args(argc, argv, &args))
 		return -1;
@@ -154,12 +223,12 @@ int main(int argc, char **argv)
 	size = args.size;
 	access_type = args.access_type;
 
-	perf_init();
+	po = perf_init();
 	init_object(&object, size, access_type);
-	perf_record_start();
+	perf_record_start(&po);
 	access_object(object, size);
-	perf_record_end();
-	perf_report();
+	perf_record_end(&po);
+	perf_report(&po);
 
 	return 0;
 }
