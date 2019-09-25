4fc7edb9c791ae4ec25ef83c31d21e20286b2caf
Yunjae Lee <lyj7694@gmail.com>
2019-09-25 10:11:15 +0900

    Limit the memory when accessing objects
    Now we can simulate the various size of memory.
    
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- overhead/measure.sh
+++ overhead/measure.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]
+if [ "$#" -ne 3 ]
 then
-	echo "Usage: $0 <seq|rand> <Object size (MiB)>"
+	echo "Usage: $0 <seq|rand> <Memory size (MiB)> <Object size (MiB)>"
 	exit
 fi
 
 TYPE=$1
-SIZE=$2
+MEM=$2
+SIZE=$3
 
-./generate $TYPE $SIZE && ./access $TYPE $SIZE
+./generate $TYPE $SIZE && ./run_memcg_lim.sh $MEM "./access $TYPE $SIZE"
---
+++ overhead/run_memcg_lim.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Run a command with memory cgroup that has specific memory limit
+# Forked from https://github.com/sjp38/lazybox
+
+if [ $# -ne 2 ]
+then
+	echo "Usage: $0 <mem limit in MiB> <command>"
+	exit 1
+fi
+
+MEMLIM=`printf '%.0f\n' "$(echo "scale=1; $1*1024*1024" | bc -l)"`
+COMM=$2
+
+MEMCG_ORIG_DIR=/sys/fs/cgroup/memory/
+MEMCG_DIR=/sys/fs/cgroup/memory/run_memcg_lim_$USER
+
+if [ -d "$MEMCG_DIR" ]
+then
+	sudo rmdir $MEMCG_DIR
+fi
+
+sudo mkdir $MEMCG_DIR
+sudo bash -c "echo $$ > $MEMCG_DIR/tasks"
+sudo bash -c "echo $MEMLIM > $MEMCG_DIR/memory.limit_in_bytes"
+
+echo "COMM: $COMM"
+
+eval "$COMM"
+
+while read pid; do
+	sudo bash -c "echo $pid > $MEMCG_ORIG_DIR/tasks"
+done < $MEMCG_DIR/tasks
+
+sudo rmdir $MEMCG_DIR
